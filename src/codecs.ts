9a68463e9d8dfd79be04ffb079ea7607ade0a070
michaelfarrell76 <mike@transcend.io>
2022-01-21 22:56:08 -0800

    Adds initial codec to validate yml schema

--- src/codecs.ts
+++ src/codecs.ts
@@ -1,4 +1,10 @@
 import * as t from 'io-ts';
+import { valuesOf } from '@transcend-io/type-utils';
+import {
+  DataCategoryType,
+  ProcessingPurpose,
+  RequestAction,
+} from '@transcend-io/privacy-types';
 
 /**
  * Input to define an enricher
@@ -38,6 +44,28 @@ export const EnricherInput = t.intersection([
 export type EnricherInput = t.TypeOf<typeof EnricherInput>;
 
 /**
+ * Annotate specific fields within an object. These are often database table columns.
+ * Fields can also be a JSON object or separate file.
+ */
+export const FieldInput = t.intersection([
+  t.type({
+    /** The unique key of the field */
+    key: t.string,
+  }),
+  t.partial({
+    /** Display title of the field */
+    title: t.string,
+    /** The unique key of the object. When a database, this is the table name. */
+    key: t.string,
+    /** Description of the field */
+    description: t.string,
+  }),
+]);
+
+/** Type override */
+export type FieldInput = t.TypeOf<typeof FieldInput>;
+
+/**
  * Objects are the different types of data models that existing within your data silo.
  * If the data silo is a database, these would be your tables.
  * Note: These are currently called "datapoints" in the Transcend UI and documentation.
@@ -54,6 +82,17 @@ export const ObjectInput = t.intersection([
     description: t.string,
     /** The unique key of the object. When a database, this is the table name. */
     key: t.string,
+    /** What is the purpose of processing for this object/table? */
+    purpose: valuesOf(ProcessingPurpose),
+    /** The category of personal data for this object */
+    category: valuesOf(DataCategoryType),
+    /** The types of privacy actions that this object can implement */
+    'privacy-actions': t.array(valuesOf(RequestAction)),
+    /**
+     * Provide field-level metadata for this object.
+     * This is often the column metadata
+     */
+    fields: t.array(FieldInput),
   }),
 ]);
 
@@ -113,62 +152,6 @@ export const DataSiloInput = t.intersection([
 
 /** Type override */
 export type DataSiloInput = t.TypeOf<typeof DataSiloInput>;
-
-//         # What is the purpose of processing for this object/table?
-//         #   ESSENTIAL: Provide a service that the user explicitly requests and that is part of the product's basic service or functionality
-//         #   ADDITIONAL_FUNCTIONALITY: Provide a service that the user explicitly requests but that is not a necessary part of the product's basic service
-//         #   ADVERTISING: To show ads that are either targeted to the specific user or not targeted
-//         #   MARKETING: To contact the user to offer products, services, or other promotions
-//         #   ANALYTICS: For understanding the product’s audience, improving the product, inform company strategy, or general research
-//         #   PERSONALIZATION: For providing user with a personalized experience
-//         #   OPERATION_SECURITY: For product operation and security, enforcement of terms of service, fraud prevention, protecting users and property, etc.
-//         #   LEGAL: For compliance with legal obligations
-//         #   TRANSFER: For data that was transferred as part of a change in circumstance (e.g. a merger or acquisition)
-//         #   SALE: For selling the data to third parties
-//         #   HR: For personnel training, recruitment, payroll, management, etc.
-//         #   OTHER: Other specific purpose not covered above
-//         #   UNSPECIFIED: The purpose is not explicitly stated or is unclear
-//         purpose: ESSENTIAL
-//         # The category of personal data for this object
-//         #   FINANCIAL: Financial information
-//         #   HEALTH: Health information
-//         #   CONTACT: Contact information
-//         #   LOCATION: Geo-location information
-//         #   DEMOGRAPHIC: Demographic Information
-//         #   ID: Identifiers that uniquely identify a person
-//         #   ONLINE_ACTIVITY: The user's online activities on the first party website/app or other websites/apps
-//         #   USER_PROFILE: The user’s profile on the first-party website/app and its contents
-//         #   SOCIAL_MEDIA: User profile and data from a social media website/app or other third party service
-//         #   CONNECTION: Connection information for the current browsing session, e.g. device IDs, MAC addresses, IP addresses, etc.
-//         #   TRACKING: Cookies and tracking elements
-//         #   DEVICE: Computer or device information
-//         #   SURVEY: Any data that is collected through surveys
-//         #   OTHER: A specific type of information not covered by the above categories
-//         #   UNSPECIFIED: The type of information is not explicitly stated or unclear
-//         category: USER_PROFILE
-//         # The types of privacy actions that this object can implement
-//         #   AUTOMATED_DECISION_MAKING_OPT_OUT: Opt out of automated decision making
-//         #   CONTACT_OPT_OUT: Opt out of all communication
-//         #   SALE_OPT_OUT: Opt-out of the sale of personal data
-//         #   TRACKING_OPT_OUT: Opt out of tracking
-//         #   ACCESS: Data Download request
-//         #   ERASURE: Erase the profile from the system
-//         #   ACCOUNT_DELETION: Run an account deletion instead of a fully compliant deletion
-//         #   RECTIFICATION: Make an update to an inaccurate record
-//         #   RESTRICTION: Restrict processing
-//         #   DATA_CHECK: Check for data (after an erasure request)
-//         privacy-actions:
-//           - ACCESS
-//           - ERASURE
-//           - CONTACT_OPT_OUT
-//         # Provide field-level metadata for this object.
-//         fields:
-//           - key: firstName
-//             title: First Name
-//             description: The first name of the user, inputted during onboarding
-//           - key: email
-//             title: Email
-//             description: The email address of the user
 
 export const TranscendInput = t.partial({
   /**
