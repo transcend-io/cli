eceb83fe37892aa1ebee9ba18c519dca68dae352
Michael Farrell <mkfrl09@gmail.com>
2022-01-22 19:17:26 -0500

    Adds shell of main script with one example GraphQL request (#3)

---
+++ src/tests/main.test.ts
@@ -0,0 +1,4 @@
+// TODO: https://transcend.height.app/T-10530
+// yarn ts-node ./src/main.ts --file=./examples/simple.yml
+// yarn ts-node ./src/main.ts --file=./examples/invalid.yml
+// yarn ts-node ./src/main.ts
--- src/tests/readTranscendYaml.test.ts
+++ src/tests/readTranscendYaml.test.ts
@@ -1,30 +1,20 @@
 import { expect } from 'chai';
 import { join } from 'path';
-import { decodeCodec } from '@transcend-io/type-utils';
-import yaml from 'js-yaml';
-import { readFileSync } from 'fs';
 
-import { TranscendInput } from '../index';
+import { readTranscendYaml } from '../index';
 
 const EXAMPLE_DIR = join(__dirname, '..', '..', 'examples');
 
-describe('codecs', () => {
+describe('readTranscendYaml', () => {
   it('simple.yml should pass the codec validation for TranscendInput', () => {
     expect(() =>
-      decodeCodec(
-        TranscendInput,
-        yaml.load(readFileSync(join(EXAMPLE_DIR, 'simple.yml'), 'utf-8')),
-      ),
+      readTranscendYaml(join(EXAMPLE_DIR, 'simple.yml')),
     ).to.not.throw();
   });
 
   it('invalid.yml should fail the codec validation for TranscendInput', () => {
-    expect(() =>
-      decodeCodec(
-        TranscendInput,
-        yaml.load(readFileSync(join(EXAMPLE_DIR, 'invalid.yml'), 'utf-8')),
-      ),
-    ).to.throw(`".enrichers.0.0.title expected type 'string'",
+    expect(() => readTranscendYaml(join(EXAMPLE_DIR, 'invalid.yml'))).to
+      .throw(`".enrichers.0.0.title expected type 'string'",
   ".enrichers.1.0.input-identifier expected type 'string'",
   ".enrichers.1.0.output-identifiers expected type 'Array<string>'",
   ".data-silos.0.0.title expected type 'string'",
