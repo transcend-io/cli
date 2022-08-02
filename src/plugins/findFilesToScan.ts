1da32fa97dcbb89a811be3c3c8802087d1b72042
Jason Wahjudi <42789873+jwahjudi@users.noreply.github.com>
2022-08-02 13:59:25 -0700

    Allow All Types of .txt Files For Python Requirements.txt (#49)
    * Allow all types of .txt files
    
    * Added fileGlobs as an argument
    
    * small syntax changes
    
    Co-authored-by: jwahjudi <jason@transcend.io>

--- src/plugins/findFilesToScan.ts
+++ src/plugins/findFilesToScan.ts
@@ -5,33 +5,45 @@ import { SiloDiscoveryConfig, SiloDiscoveryRawResults } from '.';
  * Helper to scan for data silos in all package.json files that it can find in a directory
  *
  * @param scanPath - Where to look for package.json files
+ * @param fileGlobs - Globs to look for
  * @param ignoreDirs - The directories to ignore (excludes node_modules and serverless-build)
  * @param config - Silo Discovery configuration
  * @returns the list of integrations
  */
 export const findFilesToScan = async (
   scanPath: string,
+  fileGlobs: string,
   ignoreDirs: string,
   config: SiloDiscoveryConfig,
 ): Promise<SiloDiscoveryRawResults[]> => {
   const { ignoreDirs: IGNORE_DIRS, supportedFiles, scanFunction } = config;
+  const globsToSupport =
+    fileGlobs === ''
+      ? supportedFiles
+      : supportedFiles.concat(fileGlobs.split(','));
   const dirsToIgnore = [...ignoreDirs.split(','), ...IGNORE_DIRS].filter(
     (dir) => dir.length > 0,
   );
-  const filesToScan: string[] = await fastGlob(
-    `${scanPath}/**/${supportedFiles.join('|')}`,
-    {
-      ignore: dirsToIgnore.map((dir: string) => `${scanPath}/**/${dir}`),
-      unique: true,
-      onlyFiles: true,
-    },
-  );
-  const allDeps = filesToScan
-    .map((filePath: string) => scanFunction(filePath))
-    .flat();
-  const uniqueDeps = new Set(allDeps);
-  return [...uniqueDeps].map((dep) => ({
-    name: dep,
-    resourceId: `${scanPath}/**/${dep}`,
-  }));
+  try {
+    const filesToScan: string[] = await fastGlob(
+      `${scanPath}/**/${globsToSupport.join('|')}`,
+      {
+        ignore: dirsToIgnore.map((dir: string) => `${scanPath}/**/${dir}`),
+        unique: true,
+        onlyFiles: true,
+      },
+    );
+    const allDeps = filesToScan
+      .map((filePath: string) => scanFunction(filePath))
+      .flat();
+    const uniqueDeps = new Set(allDeps);
+    return [...uniqueDeps].map((dep) => ({
+      name: dep,
+      resourceId: `${scanPath}/**/${dep}`,
+    }));
+  } catch (error) {
+    throw new Error(
+      `Error scanning globs ${findFilesToScan} with error: ${error}`,
+    );
+  }
 };
