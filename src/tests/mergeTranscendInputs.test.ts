fe79df5b6421941f0c576c2643d084a3b7742da8
Michael Farrell <mike@transcend.io>
2022-10-24 10:12:31 -0700

    Properly merges yaml files when syncing multiple (#79)

---
+++ src/tests/mergeTranscendInputs.test.ts
@@ -0,0 +1,111 @@
+import { expect } from 'chai';
+
+import { mergeTranscendInputs, TranscendInput } from '../index';
+
+describe('mergeTranscendInputs', () => {
+  const test: TranscendInput = {
+    'data-silos': [
+      {
+        title: 'Test A',
+        integrationName: 'server',
+      },
+    ],
+  };
+
+  const test2: TranscendInput = {
+    'data-silos': [
+      {
+        title: 'Test B',
+        integrationName: 'server',
+      },
+    ],
+    enrichers: [
+      {
+        title: 'Test Enricher A',
+        url: 'https://test.trancsend.io',
+        'input-identifier': 'email',
+        'output-identifiers': ['email'],
+      },
+    ],
+    templates: [
+      {
+        title: 'Test Template A',
+      },
+    ],
+    'api-keys': [
+      {
+        title: 'Test API Key A',
+      },
+    ],
+  };
+
+  const merged: TranscendInput = {
+    'data-silos': [
+      {
+        title: 'Test A',
+        integrationName: 'server',
+      },
+      {
+        title: 'Test B',
+        integrationName: 'server',
+      },
+    ],
+    enrichers: [
+      {
+        title: 'Test Enricher A',
+        url: 'https://test.trancsend.io',
+        'input-identifier': 'email',
+        'output-identifiers': ['email'],
+      },
+    ],
+    templates: [
+      {
+        title: 'Test Template A',
+      },
+    ],
+    'api-keys': [
+      {
+        title: 'Test API Key A',
+      },
+    ],
+  };
+
+  const mergedReverse: TranscendInput = {
+    'data-silos': [
+      {
+        title: 'Test B',
+        integrationName: 'server',
+      },
+      {
+        title: 'Test A',
+        integrationName: 'server',
+      },
+    ],
+    enrichers: [
+      {
+        title: 'Test Enricher A',
+        url: 'https://test.trancsend.io',
+        'input-identifier': 'email',
+        'output-identifiers': ['email'],
+      },
+    ],
+    templates: [
+      {
+        title: 'Test Template A',
+      },
+    ],
+    'api-keys': [
+      {
+        title: 'Test API Key A',
+      },
+    ],
+  };
+
+  it('should merge together', () => {
+    expect(mergeTranscendInputs(test, test2)).to.deep.equal(merged);
+  });
+
+  it('should merge together in swapped order', () => {
+    expect(mergeTranscendInputs(test2, test)).to.deep.equal(mergedReverse);
+  });
+});
