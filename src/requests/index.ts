c2817756acddbd9a15bdadb6b9a265677fe6acc5
michaelfarrell76 <mike@transcend.io>
2022-12-03 12:11:24 -0800

    More helpers for request import

--- src/requests/index.ts
+++ src/requests/index.ts
@@ -1,3 +1,6 @@
-export * from './splitToCsv';
+export * from './splitCsvToList';
+export * from './constants';
 export * from './readCsv';
 export * from './getUniqueValuesForColumn';
+export * from './fuzzyMatchColumns';
+export * from './parseAttributesFromString';
