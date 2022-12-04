37fe7b75dc11bd245181dbc7f74ca9a437f59449
Michael Farrell <mike@transcend.io>
2022-12-04 02:16:20 -0800

    Adds new cli command tr-pull-cron-identifiers (#89)
    * Adds new cli command tr-pull-cron-identifiers
    
    * readme

---
+++ src/cli-pull-cron-identifiers.ts
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+import yargs from 'yargs-parser';
+import colors from 'colors';
+
+import { logger } from './logger';
+import { pullCronIdentifiersToCsv } from './cron';
+import { RequestAction } from '@transcend-io/privacy-types';
+
+/**
+ * Pull the set of active identifiers for a cron job silo.
+ *
+ * Requires an API key with scope for the cron integration being checked on.
+ *
+ * Dev Usage:
+ * yarn ts-node ./src/cli-pull-cron-identifiers.ts --auth=asd123 \
+ *   --dataSiloId=92636cda-b7c6-48c6-b1b1-2df574596cbc \
+ *   --requestType=ERASURE \
+ *   --file=/Users/michaelfarrell/Desktop/test.csv
+ *
+ * Standard usage:
+ * yarn tr-pull-cron-identifiers --auth=asd123  \
+ *   --dataSiloId=92636cda-b7c6-48c6-b1b1-2df574596cbc \
+ *   --requestType=ERASURE \
+ *   --file=/Users/michaelfarrell/Desktop/test.csv
+ */
+async function main(): Promise<void> {
+  // Parse command line arguments
+  const {
+    file = './cron-identifiers.csv',
+    transcendApiUrl = 'https://api.transcend.io',
+    auth,
+    sombraAuth,
+    dataSiloId,
+    requestType,
+    pageLimit = '100',
+  } = yargs(process.argv.slice(2)) as { [k in string]: string };
+
+  // Ensure auth is passed
+  if (!auth) {
+    logger.error(
+      colors.red(
+        'A Transcend API key must be provided. You can specify using --auth=asd123',
+      ),
+    );
+    process.exit(1);
+  }
+
+  // Validate request type
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  if (!Object.values(RequestAction).includes(requestType as any)) {
+    logger.error(
+      colors.red(
+        `Failed to parse requestType:"${requestType}".\n` +
+          `Expected one of: \n${Object.values(RequestAction).join('\n')}`,
+      ),
+    );
+    process.exit(1);
+  }
+
+  // Upload privacy requests
+  await pullCronIdentifiersToCsv({
+    file,
+    transcendApiUrl,
+    pageLimit: parseInt(pageLimit, 10),
+    requestType: requestType as RequestAction,
+    auth,
+    sombraAuth,
+    dataSiloId,
+  });
+}
+
+main();
