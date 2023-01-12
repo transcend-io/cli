507a9ca0cecc782c3f6aaa46937ce5f5cc5488cc
Michael Farrell <mike@transcend.io>
2023-01-11 21:45:04 -0800

    Adds Cli command to bulk-update request data silos as completed (#106)

---
+++ src/graphql/gqls/RequestDataSilo.ts
@@ -0,0 +1,28 @@
+import { gql } from 'graphql-request';
+
+export const REQUEST_DATA_SILOS = gql`
+  query TranscendCliRequestDataSilos($requestId: ID!, $dataSiloId: ID!) {
+    requestDataSilos(
+      filterBy: { requestId: $requestId, dataSiloId: $dataSiloId }
+    ) {
+      nodes {
+        id
+      }
+    }
+  }
+`;
+
+export const CHANGE_REQUEST_DATA_SILO_STATUS = gql`
+  mutation TranscendCliMarkRequestDataSiloCompleted(
+    $requestDataSiloId: ID!
+    $status: UpdateRequestDataSiloStatus!
+  ) {
+    changeRequestDataSiloStatus(
+      input: { id: $requestDataSiloId, status: $status }
+    ) {
+      requestDataSilo {
+        id
+      }
+    }
+  }
+`;
