a19dc75964aef2b614d70fd98f4a630935e60bc9
Michael Farrell <mike@transcend.io>
2023-04-25 00:17:17 -0700

    Handle rate limit on determine login (#158)
    * Handle rate limit on determine login
    
    * scopes

--- src/graphql/makeGraphQLRequest.ts
+++ src/graphql/makeGraphQLRequest.ts
@@ -5,6 +5,18 @@ import colors from 'colors';
 const MAX_RETRIES = 4;
 
 /**
+ * Sleep in a promise
+ *
+ * @param sleepTime - The time to sleep in milliseconds.
+ * @returns Resolves promise
+ */
+function sleepPromise(sleepTime: number): Promise<number> {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(sleepTime), sleepTime);
+  });
+}
+
+/**
  * Make a GraphQL request with retries
  *
  * @param client - GraphQL client
@@ -29,6 +41,22 @@ export async function makeGraphQLRequest<T, V = Variables>(
       const result = await client.request(document, variables, requestHeaders);
       return result;
     } catch (err) {
+      // wait for rate limit to resolve
+      if (err.message.startsWith('Client error: Too many requests')) {
+        const rateLimitResetAt = err.response.headers?.get('x-ratelimit-reset');
+        const sleepTime = rateLimitResetAt
+          ? new Date(rateLimitResetAt).getTime() - new Date().getTime() + 100
+          : 1000 * 10;
+        logger.log(
+          colors.yellow(
+            `DETECTED RATE LIMIT: ${err.message}. Sleeping for ${sleepTime}ms`,
+          ),
+        );
+
+        // eslint-disable-next-line no-await-in-loop
+        await sleepPromise(sleepTime);
+      }
+
       if (retryCount >= maxRequests) {
         throw err;
       }
