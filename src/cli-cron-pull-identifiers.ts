0ac3041ba36de0559a2d960d16b0e60cd5a2f88c
Michael Farrell <mike@transcend.io>
2023-06-27 12:40:00 -0400

    Cli command tr-skip-request-data-silos (#172)

--- src/cli-cron-pull-identifiers.ts
+++ src/cli-cron-pull-identifiers.ts
@@ -13,13 +13,13 @@ import { RequestAction } from '@transcend-io/privacy-types';
  * Requires an API key with scope for the cron integration being checked on.
  *
  * Dev Usage:
- * yarn ts-node ./src/cli-cron-pull-identifiers.ts --auth=asd123 \
+ * yarn ts-node ./src/cli-cron-pull-identifiers.ts --auth=$TRANSCEND_API_KEY \
  *   --dataSiloId=92636cda-b7c6-48c6-b1b1-2df574596cbc \
  *   --requestType=ERASURE \
  *   --file=/Users/michaelfarrell/Desktop/test.csv
  *
  * Standard usage:
- * yarn tr-cron-pull-identifiers --auth=asd123  \
+ * yarn tr-cron-pull-identifiers --auth=$TRANSCEND_API_KEY  \
  *   --dataSiloId=92636cda-b7c6-48c6-b1b1-2df574596cbc \
  *   --requestType=ERASURE \
  *   --file=/Users/michaelfarrell/Desktop/test.csv
@@ -40,7 +40,7 @@ async function main(): Promise<void> {
   if (!auth) {
     logger.error(
       colors.red(
-        'A Transcend API key must be provided. You can specify using --auth=asd123',
+        'A Transcend API key must be provided. You can specify using --auth=$TRANSCEND_API_KEY',
       ),
     );
     process.exit(1);
