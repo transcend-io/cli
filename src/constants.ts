4f2272a50d25e8fe805ade68fd6cf5ef683ffa45
Michael Farrell <mike@transcend.io>
2023-08-03 16:21:16 -0700

    Env var for transcend url (#186)

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,9 @@ export const ADMIN_DASH = 'https://app.transcend.io';
 
 export const ADMIN_DASH_INTEGRATIONS = `${ADMIN_DASH}/infrastructure/integrations`;
 
-export const DEFAULT_TRANSCEND_API = 'https://api.transcend.io';
+/**
+ * Override default transcend API url using
+ * TRANSCEND_API_URL=https://api.us.transcend.io tr-pull ...
+ */
+export const DEFAULT_TRANSCEND_API =
+  process.env.TRANSCEND_API_URL || 'https://api.transcend.io';
