f8d8def48c0f855b86dc4c198b02a04d67e41416
michaelfarrell76 <mike@transcend.io>
2023-10-02 16:29:54 -0700

    null

---
+++ src/tests/removeLinks.test.ts
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+
+import { removeLinks } from '../ai/removeLinks';
+
+const TEST_DATA = `
+"title": "move health check to /test /health https://app.datadoghq.com/logs?query=service%3Aprod-&live=true",
+"due_date": "Fri Sep 29 2023",
+"priority": "P3",
+"ticket_type": "implementation"
+}`;
+
+describe('removeLinks', () => {
+  it('should remove links', () => {
+    expect(removeLinks(TEST_DATA)).to.deep.equal(`
+"title": "move health check to /test /health <link-omitted>
+"due_date": "Fri Sep 29 2023",
+"priority": "P3",
+"ticket_type": "implementation"
+}`);
+  });
+});
