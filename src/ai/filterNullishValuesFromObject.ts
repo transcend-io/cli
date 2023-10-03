f9ed7fbe06046aa2f066d6db627042c370a3c95b
Michael Farrell <mike@transcend.io>
2023-10-03 09:46:01 -0700

    Adds helper to filter null values from an object (#230)
    * Adds helper to filter null values
    
    * filter empty list
    
    * filterNullishValuesFromObject
    
    * json2Csv

---
+++ src/ai/filterNullishValuesFromObject.ts
@@ -0,0 +1,23 @@
+import { ObjByString } from '@transcend-io/type-utils';
+
+/**
+ * Given an object, remove all keys that are null-ish
+ *
+ * @param obj - Object
+ * @returns Object with null-ish values removed
+ */
+export function filterNullishValuesFromObject<T extends ObjByString>(
+  obj: T,
+): T {
+  return Object.entries(obj).reduce(
+    (acc, [k, v]) =>
+      v !== null &&
+      v !== undefined &&
+      v !== '' &&
+      !(Array.isArray(v) && v.length === 0) &&
+      !(typeof v === 'object' && Object.keys(v).length === 0)
+        ? Object.assign(acc, { [k]: v })
+        : acc,
+    {} as T,
+  );
+}
---
+++ src/tests/filterNullValuesFromObject.test.ts
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+
+import { filterNullishValuesFromObject } from '../ai/filterNullishValuesFromObject';
+
+const TEST_DATA = {
+  test: 'data',
+  name: true,
+  cow: false,
+  key: null,
+  moose: null,
+  str: '',
+  cat: [],
+  mew: { dog: 2 },
+  woof: {},
+};
+
+describe('filterNullishValuesFromObject', () => {
+  it('should remove links', () => {
+    expect(filterNullishValuesFromObject(TEST_DATA)).to.deep.equal({
+      test: 'data',
+      name: true,
+      cow: false,
+      mew: { dog: 2 },
+    });
+  });
+});
