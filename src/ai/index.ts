3be46082544a663a04b626c2afa0ecad872c1d93
Michael Farrell <mike@transcend.io>
2023-10-03 09:46:01 -0700

    Adds helper to filter null values from an object (#230)
    * Adds helper to filter null values
    
    * filter empty list
    
    * filterNullishValuesFromObject
    
    * json2Csv

--- src/ai/index.ts
+++ src/ai/index.ts
@@ -1,3 +1,8 @@
+const { parse } = require('json2csv');
+
+export const json2Csv = parse;
+
 export * from './TranscendAiPrompt';
 export * from './createHandlebarsWithHelpers';
 export * from './removeLinks';
+export * from './filterNullishValuesFromObject';
