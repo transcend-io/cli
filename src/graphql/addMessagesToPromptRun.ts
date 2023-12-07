25f6ee80a51e37d774c99a9b521d67c0e88cd0b8
Michael Farrell <mike@transcend.io>
2023-12-06 21:25:17 -0800

    Add messages to prompt run (#274)
    * Add messages to prompt run
    
    * PathfinderPromptRunMetadata
    
    * aapp name
    
    * app user

---
+++ src/graphql/addMessagesToPromptRun.ts
@@ -0,0 +1,71 @@
+import { GraphQLClient } from 'graphql-request';
+import { ADD_MESSAGES_TO_PROMPT_RUN } from './gqls';
+import { makeGraphQLRequest } from './makeGraphQLRequest';
+import { QueueStatus, ChatCompletionRole } from '@transcend-io/privacy-types';
+
+export interface AddMessagesToPromptRunInput {
+  /** ID of run */
+  promptRunId:
+    | {
+        /** Report by prompt run name */
+        name: string;
+        /** Don't report by ID */
+        id?: undefined;
+      }
+    | {
+        /** Don't report by name */
+        name: undefined;
+        /** Report by prompt run ID */
+        id?: string;
+      };
+  /** Messages to report on */
+  promptRunMessages?: {
+    /** Message reported */
+    content: string;
+    /** Role of message */
+    role: ChatCompletionRole;
+    /** Template used if created from prompt */
+    template?: string;
+  }[];
+  /** Error message (if one exists) */
+  error?: string;
+  /** The status of the run */
+  status?: QueueStatus;
+  /** Duration of time that it took to execute the prompt */
+  duration?: number;
+}
+
+/**
+ * Record a new prompt run
+ *
+ * @param client - GraphQL client
+ * @param input - Prompt input
+ * @returns Prompt ID
+ */
+export async function addMessagesToPromptRun(
+  client: GraphQLClient,
+  { promptRunId, promptRunMessages = [], ...rest }: AddMessagesToPromptRunInput,
+): Promise<string> {
+  const {
+    addMessagesToPromptRun: { promptRun },
+  } = await makeGraphQLRequest<{
+    /** addMessagesToPromptRun mutation */
+    addMessagesToPromptRun: {
+      /** Prompt */
+      promptRun: {
+        /** ID */
+        id: string;
+      };
+    };
+  }>(client, ADD_MESSAGES_TO_PROMPT_RUN, {
+    input: {
+      ...rest,
+      ...promptRunId,
+      promptRunMessages: promptRunMessages.map(({ content, ...rest }) => ({
+        ...rest,
+        message: content,
+      })),
+    },
+  });
+  return promptRun.id;
+}
--- src/graphql/reportPromptRun.ts
+++ src/graphql/reportPromptRun.ts
@@ -5,7 +5,35 @@ import {
   QueueStatus,
   ChatCompletionRole,
   PromptRunProductArea,
+  LargeLanguageModelClient,
 } from '@transcend-io/privacy-types';
+
+/**
+ * Interface of metadata that can be passed for logging purposes
+ * via the Transcend Pathfinder
+ */
+export interface PathfinderPromptRunMetadata {
+  /** Unique name for the current prompt run */
+  promptRunName?: string;
+  /** ID of the Transcend prompt being reported */
+  promptId?: string;
+  /** Title of the prompt being reported on */
+  promptTitle?: string;
+  /** The ID of the prompt group being reported */
+  promptGroupId?: string;
+  /** The title of the prompt group being reported */
+  promptGroupTitle?: string;
+  /** Employee email that is executing the request */
+  runByEmployeeEmail?: string;
+  /** ID of the application calling pathfinder  */
+  applicationId?: string;
+  /** Name of the application calling pathfinder  */
+  applicationName?: string;
+  /** Core identifier of the application user being reported on  */
+  applicationUserCoreIdentifier?: string;
+  /** Name of the application user being reported on  */
+  applicationUserName?: string;
+}
 
 export interface ReportPromptRunInput {
   /** Name of run */
@@ -22,7 +50,9 @@ export interface ReportPromptRunInput {
     template?: string;
   }[];
   /** ID of the Transcend prompt being reported */
-  promptId: string;
+  promptId?: string;
+  /** Title of the prompt being reported on */
+  promptTitle?: string;
   /** Error message (if one exists) */
   error?: string;
   /** The status of the run */
@@ -37,10 +67,28 @@ export interface ReportPromptRunInput {
   topP?: number;
   /** Max tokens ot sample parameter used when running prompt */
   maxTokensToSample?: number;
-  /** The prompt group being reported */
+  /** The ID of the prompt group being reported */
   promptGroupId?: string;
+  /** The title of the prompt group being reported */
+  promptGroupTitle?: string;
   /** The LLM Id being reported on */
   largeLanguageModelId?: string;
+  /** The name of the large language model being reported on */
+  largeLanguageModelName?: string;
+  /** The name of the large language model client reported on */
+  largeLanguageModelClient?: LargeLanguageModelClient;
+  /** ID of the application calling pathfinder  */
+  applicationId?: string;
+  /** Name of the application calling pathfinder  */
+  applicationName?: string;
+  /** ID of the pathfinder applying policies  */
+  pathfinderId?: string;
+  /** Name of the pathfinder applying policies  */
+  pathfinderName?: string;
+  /** Core identifier of the application user being reported on  */
+  applicationUserCoreIdentifier?: string;
+  /** Name of the application user being reported on  */
+  applicationUserName?: string;
 }
 
 /**
