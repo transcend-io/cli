24fc182e6b2a3e6a012a63bc14df4057fb3163d9
Michael Farrell <mike@transcend.io>
2023-12-06 21:25:17 -0800

    Add messages to prompt run (#274)
    * Add messages to prompt run
    
    * PathfinderPromptRunMetadata
    
    * aapp name
    
    * app user

--- src/graphql/gqls/promptRun.ts
+++ src/graphql/gqls/promptRun.ts
@@ -10,3 +10,16 @@ export const REPORT_PROMPT_RUN = gql`
     }
   }
 `;
+
+export const ADD_MESSAGES_TO_PROMPT_RUN = gql`
+  mutation TranscendCliAddMessagesToPromptRun(
+    $input: AddMessagesToPromptRunInput!
+  ) {
+    addMessagesToPromptRun(input: $input) {
+      clientMutationId
+      promptRun {
+        id
+      }
+    }
+  }
+`;
