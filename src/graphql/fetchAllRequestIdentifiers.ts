593143268dc5a78b4a50acf45181c09219558383
Jeffery Hoffman <jeffery@transcend.io>
2024-04-22 22:20:18 +0000

    Throw error if Sombra not upgraded instead of degrading

--- src/graphql/fetchAllRequestIdentifiers.ts
+++ src/graphql/fetchAllRequestIdentifiers.ts
@@ -5,7 +5,7 @@ import { GraphQLClient } from 'graphql-request';
 import * as t from 'io-ts';
 import semver from 'semver';
 
-import { REQUEST_IDENTIFIERS, SOMBRA_VERSION } from './gqls';
+import { SOMBRA_VERSION } from './gqls';
 import { makeGraphQLRequest } from './makeGraphQLRequest';
 
 const MIN_SOMBRA_VERSION_TO_DECRYPT = '7.180';
@@ -72,65 +72,42 @@ export async function fetchAllRequestIdentifiers(
     };
   }>(client!, SOMBRA_VERSION);
 
-  // Null here represents multi-tenant Sombra
-  const decrypt =
-    version === null || semver.gte(version, MIN_SOMBRA_VERSION_TO_DECRYPT);
+  if (version && semver.lt(version, MIN_SOMBRA_VERSION_TO_DECRYPT)) {
+    throw new Error(
+      `Please upgrade Sombra to ${MIN_SOMBRA_VERSION_TO_DECRYPT} or greater to use this command.`,
+    );
+  }
 
   do {
     let nodes: RequestIdentifier[] = [];
 
-    if (decrypt) {
-      let response;
-
-      // Get decrypted identifiers via Sombra
-      try {
-        // eslint-disable-next-line no-await-in-loop
-        response = await sombra!
-          .post<{
-            /** Decrypted identifiers */
-            identifiers: RequestIdentifier[];
-          }>('v1/request-identifiers', {
-            json: {
-              first: PAGE_SIZE,
-              offset,
-              requestId,
-            },
-          })
-          .json();
-        ({ identifiers: nodes } = decodeCodec(
-          RequestIdentifiersResponse,
-          response,
-        ));
-      } catch (error) {
-        if (error.response.statusCode === 400) {
-          throw new Error(
-            "Transcend CLI can't reach the Sombra endpoint to decrypt the identifiers. " +
-              'Please ensure Sombra is the correct version to use this feature.',
-          );
-        }
-        throw error;
-      }
-
-      // decode response
+    // Get decrypted identifiers via Sombra
+    try {
+      // eslint-disable-next-line no-await-in-loop
+      const response = await sombra!
+        .post<{
+          /** Decrypted identifiers */
+          identifiers: RequestIdentifier[];
+        }>('v1/request-identifiers', {
+          json: {
+            first: PAGE_SIZE,
+            offset,
+            requestId,
+          },
+        })
+        .json();
       ({ identifiers: nodes } = decodeCodec(
         RequestIdentifiersResponse,
         response,
       ));
-    } else {
-      ({
-        requestIdentifiers: { nodes },
-        // eslint-disable-next-line no-await-in-loop
-      } = await makeGraphQLRequest<{
-        /** Request Identifiers */
-        requestIdentifiers: {
-          /** List */
-          nodes: RequestIdentifier[];
-        };
-      }>(client!, REQUEST_IDENTIFIERS, {
-        first: PAGE_SIZE,
-        offset,
-        requestId,
-      }));
+    } catch (error) {
+      if (error.response.statusCode === 400) {
+        throw new Error(
+          "Transcend CLI can't reach the Sombra endpoint to decrypt the identifiers. " +
+            'Please ensure Sombra is the correct version to use this feature.',
+        );
+      }
+      throw error;
     }
 
     requestIdentifiers.push(...nodes);
