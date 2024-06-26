0927d8b753e762854239cdc84629982030bec215
Michael Farrell <mike@transcend.io>
2024-06-26 08:48:28 -0700

    Restart request enrichers (#332)
    * Restart request enrichers
    
    * Updates
    
    * Working

---
+++ src/graphql/retryRequestEnricher.ts
@@ -0,0 +1,18 @@
+import { GraphQLClient } from 'graphql-request';
+import { RETRY_REQUEST_ENRICHER } from './gqls';
+import { makeGraphQLRequest } from './makeGraphQLRequest';
+
+/**
+ * Retry a request enricher
+ *
+ * @param client - GraphQL client
+ * @param id - The ID of the request enricher to restart
+ */
+export async function retryRequestEnricher(
+  client: GraphQLClient,
+  id: string,
+): Promise<void> {
+  await makeGraphQLRequest(client, RETRY_REQUEST_ENRICHER, {
+    requestEnricherId: id,
+  });
+}
