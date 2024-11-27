524554bdbd7efcd106159c07432a5ad5a0fb17d4
Ben Brook <bencmbrook@users.noreply.github.com>
2024-11-27 13:01:58 -0500

    Add latest schema file (#367)
    * Add latest schema file
    
    * docs/refrences
    
    * Update README.md
    
    * nvmrc - node 20 doesn't work w/ ts-node
    
    ---------
    
    Co-authored-by: bencmbrook <7354176+bencmbrook@users.noreply.github.com>

--- scripts/buildTranscendJsonSchema.ts
+++ scripts/buildTranscendJsonSchema.ts
@@ -5,7 +5,7 @@
  * Most IDEs will thus autodetect `transcend.yml` and apply linting/autocomplete/intellisense.
  *
  * ... or, if the YAML file is differently named, users can add this comment to the top of the YAML file:
- * `# yaml-language-server: $schema=https://raw.githubusercontent.com/transcend-io/cli/main/transcend-policy-yml-schema.json`
+ * `# yaml-language-server: $schema=https://raw.githubusercontent.com/transcend-io/cli/main/transcend-yml-schema-latest.json`
  *
  * @see https://github.com/redhat-developer/yaml-language-server#using-inlined-schema
  * @see https://json-schema.org/understanding-json-schema/basics.html
@@ -19,20 +19,23 @@ import * as packageJson from '../package.json';
 import { TranscendInput } from '../src/codecs';
 
 const majorVersion = packageJson.version.split('.')[0];
-const schemaDefaults = {
-  $schema: 'http://json-schema.org/draft-07/schema#',
-  $id: `https://raw.githubusercontent.com/transcend-io/cli/main/transcend-yml-schema-v${majorVersion}.json`,
-  title: 'transcend.yml',
-  description:
-    'Define personal data schema and Transcend config as code with the Transcend CLI.',
-};
 
-// Build the JSON schema from io-ts codec
-const jsonSchema = { ...schemaDefaults, ...toJsonSchema(TranscendInput) };
+// Create a major version JSON schema definition, and update the latest JSON schema definition.
+[`v${majorVersion}`, 'latest'].forEach((key) => {
+  const fileName = `transcend-yml-schema-${key}.json`;
 
-const schemaFilePath = join(
-  process.cwd(),
-  `transcend-yml-schema-v${majorVersion}.json`,
-);
+  const schemaDefaults = {
+    $schema: 'http://json-schema.org/draft-07/schema#',
+    $id: `https://raw.githubusercontent.com/transcend-io/cli/main/${fileName}`,
+    title: 'transcend.yml',
+    description:
+      'Define personal data schema and Transcend config as code with the Transcend CLI.',
+  };
 
-writeFileSync(schemaFilePath, `${JSON.stringify(jsonSchema, null, 2)}\n`);
+  // Build the JSON schema from io-ts codec
+  const jsonSchema = { ...schemaDefaults, ...toJsonSchema(TranscendInput) };
+
+  const schemaFilePath = join(process.cwd(), fileName);
+
+  writeFileSync(schemaFilePath, `${JSON.stringify(jsonSchema, null, 2)}\n`);
+});
