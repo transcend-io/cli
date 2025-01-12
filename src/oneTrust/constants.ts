b1c5f6b321c10b7adcb90e9ec0733296fc130211
Arthur <abrantesarthur1997@gmail.com>
2025-01-12 19:20:26 +0000

    add comments

--- src/oneTrust/constants.ts
+++ src/oneTrust/constants.ts
@@ -1,6 +1,10 @@
 import { createDefaultCodec } from '../helpers';
 import { OneTrustCombinedAssessmentCodec } from './codecs';
 
-export const DEFAULT_ONE_TRUST_COMBINED_ASSESSMENT = createDefaultCodec(
-  OneTrustCombinedAssessmentCodec,
-);
+/**
+ * An object with default values of type OneTrustCombinedAssessmentCodec. It's very
+ * valuable when converting assessments to CSV. When we flatten it, the resulting
+ * value always contains all keys that eventually we add to the header.
+ */
+export const DEFAULT_ONE_TRUST_COMBINED_ASSESSMENT: OneTrustCombinedAssessmentCodec =
+  createDefaultCodec(OneTrustCombinedAssessmentCodec);
