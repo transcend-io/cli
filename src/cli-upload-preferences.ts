626e6555aa474e044987263a20c052a66eaa651d
anotherminh <minhngoc.ln@gmail.com>
2025-03-26 14:06:44 -0400

    Add cli-chunk-csv helper (#393)
    * Add cli-chunk-csv helper
    
    * fix yarn
    
    * Fix memory issue, upgrade csv-parse
    
    * default to likely choice
    
    * rm need for extra mem
    
    * rm relax
    
    * fix async chunking issues
    
    * fix yarn
    
    * undo some changes
    
    * undo changes
    
    * undo more changes
    
    * Ignore working directory
    
    * some more fixes?
    
    * refactor, fix append issue
    
    * smaller chunk
    
    * chunking is hard
    
    * smaller
    
    * forgot to commit csv changes
    
    * append
    
    * fix misisng headers
    
    * fix header
    
    * more fixes
    
    * retry new error
    
    * smaller chunk
    
    * smaller
    
    * even smaller
    
    * Handle directory
    
    * pre-commit
    
    * Add ability to skip check for existing records, useful for initial uploads
    
    * Add warning log
    
    * pre-commit
    
    * nit
    
    * pre-commit again

--- src/cli-upload-preferences.ts
+++ src/cli-upload-preferences.ts
@@ -7,6 +7,9 @@ import { logger } from './logger';
 import { DEFAULT_TRANSCEND_API } from './constants';
 import { uploadPreferenceManagementPreferencesInteractive } from './preference-management';
 import { splitCsvToList } from './requests';
+import { readdirSync } from 'fs';
+import { map } from 'bluebird';
+import { basename, join } from 'path';
 
 /**
  * Upload consent preferences to the managed consent database
@@ -15,18 +18,20 @@ import { splitCsvToList } from './requests';
  * https://docs.transcend.io/docs/consent/reference/managed-consent-database
  *
  * Dev Usage:
- * yarn ts-node ./src/cli-upload-consent-preferences-interactive --base64EncryptionKey=$TRANSCEND_CONSENT_ENCRYPTION_KEY \
+ * yarn ts-node ./src/cli-upload-preferences --base64EncryptionKey=$TRANSCEND_CONSENT_ENCRYPTION_KEY \
  *  --base64SigningKey=$TRANSCEND_CONSENT_SIGNING_KEY --partition=4d1c5daa-90b7-4d18-aa40-f86a43d2c726
  *
  * Standard usage:
- * yarn tr-upload-consent-preferences-interactive --base64EncryptionKey=$TRANSCEND_CONSENT_ENCRYPTION_KEY \
+ * yarn tr-upload-preferences --base64EncryptionKey=$TRANSCEND_CONSENT_ENCRYPTION_KEY \
  *  --base64SigningKey=$TRANSCEND_CONSENT_SIGNING_KEY --partition=4d1c5daa-90b7-4d18-aa40-f86a43d2c726
  */
 async function main(): Promise<void> {
   // Parse command line arguments
   const {
+    /** Directory to load preferences from */
+    directory,
     /** File to load preferences from */
-    file = './preferences.csv',
+    file,
     /** Transcend URL */
     transcendUrl = DEFAULT_TRANSCEND_API,
     /** API key */
@@ -41,12 +46,16 @@ async function main(): Promise<void> {
     skipWorkflowTriggers = 'false',
     /** Whether to skip conflict updates */
     skipConflictUpdates = 'false',
+    /** Whether to skip the check for existing records. SHOULD ONLY BE USED FOR INITIAL UPLOAD */
+    skipExistingRecordCheck = 'false',
     /** Whether to skip sending emails */
     isSilent = 'true',
     /** Attributes to add to any DSR request if created */
     attributes = 'Tags:transcend-cli,Source:transcend-cli',
     /** Store resulting, continuing where left off  */
-    receiptFilepath = './preference-management-upload-receipts.json',
+    receiptFileDir = './receipts',
+    /** Number of files to process concurrently (only relevant for directory processing) */
+    concurrency = '10',
   } = yargs(process.argv.slice(2)) as { [k in string]: string };
 
   // Ensure auth is passed
@@ -70,20 +79,97 @@ async function main(): Promise<void> {
     process.exit(1);
   }
 
-  // Upload cookies
-  await uploadPreferenceManagementPreferencesInteractive({
-    receiptFilepath,
-    auth,
-    sombraAuth,
-    file,
-    partition,
-    transcendUrl,
-    skipConflictUpdates: skipConflictUpdates !== 'false',
-    skipWorkflowTriggers: skipWorkflowTriggers !== 'false',
-    isSilent: isSilent !== 'false',
-    dryRun: dryRun !== 'false',
-    attributes: splitCsvToList(attributes),
-  });
+  if (!!directory && !!file) {
+    logger.error(
+      colors.red(
+        'Cannot provide both a directory and a file. Please provide only one.',
+      ),
+    );
+    process.exit(1);
+  }
+
+  if (!file && !directory) {
+    logger.error(
+      colors.red(
+        // eslint-disable-next-line max-len
+        'A file or directory must be provided. Please provide one using --file=./preferences.csv or --directory=./preferences',
+      ),
+    );
+    process.exit(1);
+  }
+
+  const files: string[] = [];
+
+  if (directory) {
+    try {
+      const filesInDirectory = readdirSync(directory);
+      const csvFiles = filesInDirectory.filter((file) => file.endsWith('.csv'));
+
+      if (csvFiles.length === 0) {
+        logger.error(
+          colors.red(`No CSV files found in directory: ${directory}`),
+        );
+        process.exit(1);
+      }
+
+      // Add full paths for each CSV file
+      files.push(...csvFiles.map((file) => join(directory, file)));
+    } catch (err) {
+      logger.error(colors.red(`Failed to read directory: ${directory}`));
+      logger.error(colors.red((err as Error).message));
+      process.exit(1);
+    }
+  } else {
+    try {
+      // Verify file exists and is a CSV
+      if (!file.endsWith('.csv')) {
+        logger.error(colors.red('File must be a CSV file'));
+        process.exit(1);
+      }
+      files.push(file);
+    } catch (err) {
+      logger.error(colors.red(`Failed to access file: ${file}`));
+      logger.error(colors.red((err as Error).message));
+      process.exit(1);
+    }
+  }
+
+  logger.info(
+    colors.green(
+      `Processing ${files.length} consent preferences files for partition: ${partition}`,
+    ),
+  );
+  logger.debug(`Files to process: ${files.join(', ')}`);
+
+  if (skipExistingRecordCheck !== 'false') {
+    logger.info(
+      colors.bgYellow(
+        `Skipping existing record check: ${skipExistingRecordCheck}`,
+      ),
+    );
+  }
+
+  await map(
+    files,
+    async (filePath) => {
+      const fileName = basename(filePath).replace('.csv', '');
+      await uploadPreferenceManagementPreferencesInteractive({
+        receiptFilepath: join(receiptFileDir, `${fileName}-receipts.json`),
+        auth,
+        sombraAuth,
+        file: filePath,
+        partition,
+        transcendUrl,
+        skipConflictUpdates: skipConflictUpdates !== 'false',
+        skipWorkflowTriggers: skipWorkflowTriggers !== 'false',
+        skipExistingRecordCheck: skipExistingRecordCheck !== 'false',
+        isSilent: isSilent !== 'false',
+        dryRun: dryRun !== 'false',
+        attributes: splitCsvToList(attributes),
+      });
+    },
+    { concurrency: parseInt(concurrency, 10) },
+  );
 }
 
 main();
