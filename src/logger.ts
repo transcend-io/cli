13f98a7897b793e071bf569ef6d964de06b0cbf9
bencmbrook <7354176+bencmbrook@users.noreply.github.com>
2025-07-16 20:56:13 -0400

    fix proxy type

--- src/logger.ts
+++ src/logger.ts
@@ -1,13 +1,13 @@
-import colors from 'colors';
-import { bootstrap } from 'global-agent';
-import { ProxyAgent, setGlobalDispatcher } from 'undici';
-import yargs from 'yargs-parser';
+import colors from "colors";
+import { bootstrap } from "global-agent";
+import { ProxyAgent, setGlobalDispatcher } from "undici";
+import yargs from "yargs-parser";
 
 export const logger = console;
 
 // When the proxy env var of flag is specified, initiate the proxy
 const { httpProxy = process.env.http_proxy } = yargs(process.argv.slice(2));
-if (httpProxy) {
+if (httpProxy && typeof httpProxy === "string") {
   logger.info(colors.green(`Initializing proxy: ${httpProxy}`));
 
   // Use global-agent, which overrides `request` based requests
