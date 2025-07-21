7de1181c182b29d6cd0ee335cf8f1bdf6bc60e9b
bencmbrook <7354176+bencmbrook@users.noreply.github.com>
2025-07-21 16:18:56 -0400

    migrate generate-api-keys

--- README.md
+++ README.md
@@ -1461,20 +1461,68 @@ FLAGS
 
 #### Usage
 
-```sh
-transcend admin generate-api-keys --email=test@transcend.io --password=$TRANSCEND_PASSWORD \
-   --scopes="View Email Templates,View Data Map" --apiKeyTitle="CLI Usage Cross Instance Sync" -file=./working/auth.json
-```
-
-Specifying the backend URL, needed for US hosted backend infrastructure.
+**Generate API keys for cross-instance usage**
 
 ```sh
-transcend admin generate-api-keys --email=test@transcend.io --password=$TRANSCEND_PASSWORD \
-   --scopes="View Email Templates,View Data Map" --apiKeyTitle="CLI Usage Cross Instance Sync" -file=./working/auth.json \
-   --transcendUrl=https://api.us.transcend.io
+transcend admin generate-api-keys \
+  --email=test@transcend.io \
+  --password="$TRANSCEND_PASSWORD" \
+  --scopes="View Email Templates,View Data Map" \
+  --apiKeyTitle="CLI Usage Cross Instance Sync" \
+  --file=./working/auth.json
 ```
 
-Filter for only a specific organization by ID, returning all child accounts associated with that organization. Can use the following GQL query on the [EU GraphQL Playground](https://api.us.transcend.io/graphql) or [US GraphQL Playground](https://api.us.transcend.io/graphql).
+**Specifying the backend URL, needed for US hosted backend infrastructure**
+
+```sh
+transcend admin generate-api-keys \
+  --email=test@transcend.io \
+  --password="$TRANSCEND_PASSWORD" \
+  --scopes="View Email Templates,View Data Map" \
+  --apiKeyTitle="CLI Usage Cross Instance Sync" \
+  --file=./working/auth.json \
+  --transcendUrl=https://api.us.transcend.io
+```
+
+**Filter for only a specific organization by ID, returning all child accounts associated with that organization**
+
+```sh
+transcend admin generate-api-keys \
+  --email=test@transcend.io \
+  --password="$TRANSCEND_PASSWORD" \
+  --scopes="View Email Templates,View Data Map" \
+  --apiKeyTitle="CLI Usage Cross Instance Sync" \
+  --file=./working/auth.json \
+  --parentOrganizationId=7098bb38-070d-4f26-8fa4-1b61b9cdef77
+```
+
+**Delete all API keys with a certain title**
+
+```sh
+transcend admin generate-api-keys \
+  --email=test@transcend.io \
+  --password="$TRANSCEND_PASSWORD" \
+  --scopes="View Email Templates,View Data Map" \
+  --apiKeyTitle="CLI Usage Cross Instance Sync" \
+  --file=./working/auth.json \
+  --createNewApiKey=false
+```
+
+**Throw error if an API key already exists with that title, default behavior is to delete the existing API key and create a new one with that same title**
+
+```sh
+transcend admin generate-api-keys \
+  --email=test@transcend.io \
+  --password="$TRANSCEND_PASSWORD" \
+  --scopes="View Email Templates,View Data Map" \
+  --apiKeyTitle="CLI Usage Cross Instance Sync" \
+  --file=./working/auth.json \
+  --deleteExistingApiKey=false
+```
+
+**Find your organization ID**
+
+You can use the following GQL query on the [EU GraphQL Playground](https://api.us.transcend.io/graphql) or [US GraphQL Playground](https://api.us.transcend.io/graphql) to get your organization IDs and their parent/child relationships.
 
 ```gql
 query {
@@ -1485,28 +1533,6 @@ query {
     }
   }
 }
-```
-
-```sh
-transcend admin generate-api-keys  --email=test@transcend.io --password=$TRANSCEND_PASSWORD \
-   --scopes="View Email Templates,View Data Map" --apiKeyTitle="CLI Usage Cross Instance Sync" -file=./working/auth.json \
-   --parentOrganizationId=7098bb38-070d-4f26-8fa4-1b61b9cdef77
-```
-
-Delete all API keys with a certain title.
-
-```sh
-transcend admin generate-api-keys  --email=test@transcend.io --password=$TRANSCEND_PASSWORD \
-   --scopes="View Email Templates,View Data Map" --apiKeyTitle="CLI Usage Cross Instance Sync" -file=./working/auth.json \
-   --createNewApiKey=false
-```
-
-Throw error if an API key already exists with that title, default behavior is to delete the existing API key and create a new one with that same title.
-
-```sh
-transcend admin generate-api-keys  --email=test@transcend.io --password=$TRANSCEND_PASSWORD \
-   --scopes="View Email Templates,View Data Map" --apiKeyTitle="CLI Usage Cross Instance Sync" -file=./working/auth.json \
-   --deleteExistingApiKey=false
 ```
 
 ### `transcend migration sync-ot`
--- src/commands/admin/generate-api-keys/readme.ts
+++ src/commands/admin/generate-api-keys/readme.ts
@@ -1,19 +1,87 @@
+import { buildExamples } from '../../../lib/docgen/buildExamples';
+import { ScopeName, TRANSCEND_SCOPES } from '@transcend-io/privacy-types';
+import type { GenerateApiKeysCommandFlags } from './impl';
+
+const examples = buildExamples<GenerateApiKeysCommandFlags>(
+  ['admin', 'generate-api-keys'],
+  [
+    {
+      description: 'Generate API keys for cross-instance usage',
+      flags: {
+        email: 'test@transcend.io',
+        password: '$TRANSCEND_PASSWORD',
+        scopes: `${TRANSCEND_SCOPES[ScopeName.ViewEmailTemplates].title},${
+          TRANSCEND_SCOPES[ScopeName.ViewDataMap].title
+        }`,
+        apiKeyTitle: 'CLI Usage Cross Instance Sync',
+        file: './working/auth.json',
+      },
+    },
+    {
+      description:
+        'Specifying the backend URL, needed for US hosted backend infrastructure',
+      flags: {
+        email: 'test@transcend.io',
+        password: '$TRANSCEND_PASSWORD',
+        scopes: `${TRANSCEND_SCOPES[ScopeName.ViewEmailTemplates].title},${
+          TRANSCEND_SCOPES[ScopeName.ViewDataMap].title
+        }`,
+        apiKeyTitle: 'CLI Usage Cross Instance Sync',
+        file: './working/auth.json',
+        transcendUrl: 'https://api.us.transcend.io',
+      },
+    },
+    {
+      description:
+        'Filter for only a specific organization by ID, returning all child accounts associated with that organization',
+      flags: {
+        email: 'test@transcend.io',
+        password: '$TRANSCEND_PASSWORD',
+        scopes: `${TRANSCEND_SCOPES[ScopeName.ViewEmailTemplates].title},${
+          TRANSCEND_SCOPES[ScopeName.ViewDataMap].title
+        }`,
+        apiKeyTitle: 'CLI Usage Cross Instance Sync',
+        file: './working/auth.json',
+        parentOrganizationId: '7098bb38-070d-4f26-8fa4-1b61b9cdef77',
+      },
+    },
+    {
+      description: 'Delete all API keys with a certain title',
+      flags: {
+        email: 'test@transcend.io',
+        password: '$TRANSCEND_PASSWORD',
+        scopes: `${TRANSCEND_SCOPES[ScopeName.ViewEmailTemplates].title},${
+          TRANSCEND_SCOPES[ScopeName.ViewDataMap].title
+        }`,
+        apiKeyTitle: 'CLI Usage Cross Instance Sync',
+        file: './working/auth.json',
+        createNewApiKey: 'false',
+      },
+    },
+    {
+      description:
+        'Throw error if an API key already exists with that title, default behavior is to delete the existing API key and create a new one with that same title',
+      flags: {
+        email: 'test@transcend.io',
+        password: '$TRANSCEND_PASSWORD',
+        scopes: `${TRANSCEND_SCOPES[ScopeName.ViewEmailTemplates].title},${
+          TRANSCEND_SCOPES[ScopeName.ViewDataMap].title
+        }`,
+        apiKeyTitle: 'CLI Usage Cross Instance Sync',
+        file: './working/auth.json',
+        deleteExistingApiKey: 'false',
+      },
+    },
+  ],
+);
+
 export default `#### Usage
 
-\`\`\`sh
-transcend admin generate-api-keys --email=test@transcend.io --password=$TRANSCEND_PASSWORD \\
-   --scopes="View Email Templates,View Data Map" --apiKeyTitle="CLI Usage Cross Instance Sync" -file=./working/auth.json
-\`\`\`
+${examples}
 
-Specifying the backend URL, needed for US hosted backend infrastructure.
+**Find your organization ID**
 
-\`\`\`sh
-transcend admin generate-api-keys --email=test@transcend.io --password=$TRANSCEND_PASSWORD \\
-   --scopes="View Email Templates,View Data Map" --apiKeyTitle="CLI Usage Cross Instance Sync" -file=./working/auth.json \\
-   --transcendUrl=https://api.us.transcend.io
-\`\`\`
-
-Filter for only a specific organization by ID, returning all child accounts associated with that organization. Can use the following GQL query on the [EU GraphQL Playground](https://api.us.transcend.io/graphql) or [US GraphQL Playground](https://api.us.transcend.io/graphql).
+You can use the following GQL query on the [EU GraphQL Playground](https://api.us.transcend.io/graphql) or [US GraphQL Playground](https://api.us.transcend.io/graphql) to get your organization IDs and their parent/child relationships.
 
 \`\`\`gql
 query {
@@ -24,26 +92,4 @@ query {
     }
   }
 }
-\`\`\`
-
-\`\`\`sh
-transcend admin generate-api-keys  --email=test@transcend.io --password=$TRANSCEND_PASSWORD \\
-   --scopes="View Email Templates,View Data Map" --apiKeyTitle="CLI Usage Cross Instance Sync" -file=./working/auth.json \\
-   --parentOrganizationId=7098bb38-070d-4f26-8fa4-1b61b9cdef77
-\`\`\`
-
-Delete all API keys with a certain title.
-
-\`\`\`sh
-transcend admin generate-api-keys  --email=test@transcend.io --password=$TRANSCEND_PASSWORD \\
-   --scopes="View Email Templates,View Data Map" --apiKeyTitle="CLI Usage Cross Instance Sync" -file=./working/auth.json \\
-   --createNewApiKey=false
-\`\`\`
-
-Throw error if an API key already exists with that title, default behavior is to delete the existing API key and create a new one with that same title.
-
-\`\`\`sh
-transcend admin generate-api-keys  --email=test@transcend.io --password=$TRANSCEND_PASSWORD \\
-   --scopes="View Email Templates,View Data Map" --apiKeyTitle="CLI Usage Cross Instance Sync" -file=./working/auth.json \\
-   --deleteExistingApiKey=false
 \`\`\``;
